df8d4a1544b0c9256f03a9db2332123d6135f49b
ShivamBansal96 <bansal.shivam1216@gmail.com>
2024-03-24 18:06:30 +0530

    day - 11

---
+++ src/screen/Homepage.jsx
@@ -0,0 +1,14 @@
+import "./Homepage.css";
+
+function Homepage() {
+
+
+
+    return(
+        <div id="homepage">
+            <iframe src="https://random-password-generator-eight-sooty.vercel.app/" frameborder="2"></iframe>
+        </div>
+    )
+}
+
+export default Homepage;
